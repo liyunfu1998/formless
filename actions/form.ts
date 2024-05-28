38f34186ab2bba9c66f84fbe5453a257d0c5e90f
liyunfu1998 <liyunfu1998@qq.com>
2024-05-28 20:06:30 +0800

    feat: statsCard and prisma

---
+++ actions/form.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+class UserNotFoundError extends Error {}
+
+export async function GetFormStats() {
+  const user = currentUser();
+
+  if (!user) {
+    throw new UserNotFoundError();
+  }
+
+  const stats = prisma.form.aggregate({
+    where: {
+      userId: user.id,
+    },
+    _sum: {
+      visits: true,
+      submissions: true,
+    },
+  });
+
+  const visits = stats?._sum?.visits || 0;
+  const submissions = stats?._sum?.submissions || 0;
+
+  let submissionRate = 0;
+
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+
+  const bounceRate = 100 - submissionRate;
+
+  return {
+    visits,
+    submissions,
+    submissionRate,
+    bounceRate,
+  };
+}
