cc8f97b82203f14785cd6904c01d5897999044b0
liyunfu1998 <liyunfu1998@qq.com>
2024-06-07 12:45:39 +0800

    feat: add aichat init

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,15 @@
+import AiChat from "@/components/AiChat";
 import Logo from "@/components/Logo";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
 import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import React, { ReactNode } from "react";
+import { BsRobot } from "react-icons/bs";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -10,6 +17,15 @@ export default function Layout({ children }: { children: ReactNode }) {
       <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
         <Logo />
         <div className="flex gap-4 items-center">
+          <Popover>
+            <PopoverTrigger>
+              <BsRobot className="w-6 h-6" />
+            </PopoverTrigger>
+            <PopoverContent>
+              <AiChat />
+            </PopoverContent>
+          </Popover>
+
           <ThemeSwitcher />
           <SignedIn>
             <UserButton />
---
+++ components/AiChat.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function AiChat() {
+  return (
+    <div>
+      <p>敬请期待</p>
+    </div>
+  );
+}
